e8565f4e91596ca4a5e985ef7ff6df6b9af23351
BartTheScrivner <dframe22@gmail.com>
2020-10-03 14:52:22 -0400

    setup redux store

--- src/index.js
+++ src/index.js
@@ -1,13 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'semantic-ui-css/semantic.min.css'
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk'
+
+const rootReducer = combineReducers({
+  user: userReducer,
+  projects: projectReducer,
+  sessions: sessionReducer
+})
+
+const store = createStore(
+  rootReducer, compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ 
+    && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <App store={store} />
+  </Provider>,
   document.getElementById('root')
 );
 
