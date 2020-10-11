d1b5a8a24f4d7e7e3f1bd50c656319596f6daa0d
BartTheScrivner <dframe22@gmail.com>
2020-10-10 20:46:24 -0400

    pause working on timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -3,21 +3,21 @@ import React, { useState, useRef } from "react";
 export default function Timer() {
   const [start, setStart] = useState(0);
   const [count, setCount] = useState(0);
-  const [current, setCurrent] = useState(0);
+  const [total, setTotal] = useState(0);
   const [counting, setCounting] = useState(false);
   const counter = useRef(null);
 
   const toggleCounter = () => {
-    if (!start) {
+    if (!counting) {
       setStart(new Date().getTime());
       setCount(new Date().getTime());
-    }
-    if (!counting) {
       setCounting(true);
       counter.current = setInterval(handleTimer, 1000);
     } else {
+      setStart(0);
+      setCount(0);
       setCounting(false);
-      setCurrent(current + (count - start));
+      setTotal(total + (count - start));
       clearInterval(counter.current);
     }
   };
@@ -27,7 +27,7 @@ export default function Timer() {
   }
 
   const formatTime = () => {
-    const difference = (count || 0) - start
+    const difference = ((count || 0) - start) + total
     const getSeconds = `0${Math.floor((difference % (1000 * 60)) / 1000)}`.slice(-2);
     const getMinutes = `0${Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60))}`.slice(-2);
     const getHours = `0${Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))}`.slice(-2);
