8e9ed810fa5e25d421d5c98c000df2fc8a54b136
BartTheScrivner <dframe22@gmail.com>
2020-10-10 20:36:31 -0400

    timer starts right but doesn't truly pause

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,26 +1,46 @@
-import React, {useState} from 'react'
+import React, { useState, useRef } from "react";
 
 export default function Timer() {
-  let [count, setCount] = useState(0)
-  const [counter, setCounter] = useState(null)
-  const [counting, setCounting] = useState(false)
+  const [start, setStart] = useState(0);
+  const [count, setCount] = useState(0);
+  const [current, setCurrent] = useState(0);
+  const [counting, setCounting] = useState(false);
+  const counter = useRef(null);
+
   const toggleCounter = () => {
+    if (!start) {
+      setStart(new Date().getTime());
+      setCount(new Date().getTime());
+    }
     if (!counting) {
-      setCounter(setInterval(() => {
-        setCount(count+=1)
-      }, 1000))
-      setCounting(true)
+      setCounting(true);
+      counter.current = setInterval(handleTimer, 1000);
     } else {
-    clearInterval(counter)
-    setCounting(false)
+      setCounting(false);
+      setCurrent(current + (count - start));
+      clearInterval(counter.current);
+    }
+  };
+
+  const handleTimer = () => {
+    setCount(new Date().getTime())
   }
-}
+
+  const formatTime = () => {
+    const difference = (count || 0) - start
+    const getSeconds = `0${Math.floor((difference % (1000 * 60)) / 1000)}`.slice(-2);
+    const getMinutes = `0${Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60))}`.slice(-2);
+    const getHours = `0${Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60))}`.slice(-2);
+
+    return `${getHours} : ${getMinutes} : ${getSeconds}`;
+  };
+
   return (
     <div>
-      <h2 className="count">
-        {count}
-      </h2>
-  <button onClick={toggleCounter}>{counting === false ? "START" : "PAUSE"}</button>
+      <h2 className="count">{formatTime()}</h2>
+      <button onClick={toggleCounter}>
+        {counting === false ? "START" : "PAUSE"}
+      </button>
     </div>
-  )
+  );
 }
