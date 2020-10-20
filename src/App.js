f5f718a1d2c4ca05245647942c6655ab409925b9
BartTheScrivner <dframe22@gmail.com>
2020-10-19 21:55:18 -0400

    added router-dom and routes to App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
 import React from 'react';
-import Task from './components/Task'
-import logo from './logo.svg';
+import { BrowserRouter as Router, Route, Redirect } from 'react-router-dom'
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-          <Task/>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <Router>
+        <NavBar />
+          <Route
+            exact
+            path="/"
+            render={
+              this.props.user.loggedIn
+                ? () => <Redirect to="/projects" />
+                : () => <Welcome />
+            }
+          />
+          <Route
+            exact
+            path="/projects"
+            render={
+              !this.props.user.loggedIn
+                ? () => <Redirect to="/" />
+                : (rProps) => (<ProfileContainer {...rProps}/>)
+            }
+          />
+          <Route
+            path="/insights"
+            render={
+              !this.props.user.loggedIn
+                ? () => <Redirect to="/" />
+                : (rProps) => (<DiscoverContainer {...rProps}/>)
+            }
+          />
+          <Route
+            path="/projects/:id"
+            render={
+              !this.props.user.loggedIn
+                ? () => <Redirect to="/" />
+                : (rProps) => (<FriendsContainer {...rProps}/>)
+            }
+          />
+      </Router>
+  )
 }
 
 export default App;
