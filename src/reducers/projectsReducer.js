1216e82ef1e314a301730ec595e93e13f4c04a1b
DevDaveFrame <dframe22@gmail.com>
2021-01-05 21:07:09 -0500

    set up basic projectsReducer

---
+++ src/reducers/projectsReducer.js
@@ -0,0 +1,32 @@
+const initialState = {
+  
+}
+
+export default function projectsReducer(state = initialState, action) {
+  const {type, project, projects} = action.data
+  switch (type) {
+    case "SET_USER":
+      return {
+        ...state,
+        ...projects
+      };
+    case "ADD_PROJECT":
+      return {
+        ...state,
+        ...project
+      }
+    case "UPDATE_PROJECT":
+      return {
+        ...state,
+        projects[project.id]: {
+          ...projects[project.id],
+          project
+        }
+      }
+    case "LOGOUT_USER":
+      localStorage.clear()
+      return initialState;
+    default:
+      return state;
+    }
+  }
