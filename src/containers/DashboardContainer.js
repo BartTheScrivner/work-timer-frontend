8e1225dc7e1803bf151a1d93ea918c514950395e
DevDaveFrame <dframe22@gmail.com>
2021-01-19 21:35:50 -0500

    Fixed header and add project button for DashboardContainer.js

--- src/containers/DashboardContainer.js
+++ src/containers/DashboardContainer.js
@@ -1,16 +1,24 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import {Icon} from 'semantic-ui-react'
 import ProjectThumb from '../components/ProjectThumb/ProjectThumb'
 
 export const Dashboard = (props) => {
   const {projects} = props
   return (
     <div className="container">
-      <h1>Projects Dashboard</h1>
+      <div className="row-nowrap dashboard-header">
+        <span className='large label'>Projects Dashboard</span>
+        <span className={'containable right-labeled-icon'}>
+          <Icon name="plus" size='large'/>
+          <span className='medium label'>Add a New Project</span>
+        </span>
+      </div>
+      <hr/>
       {projects
-      ? projects.forEach(project => <ProjectThumb project={project}/>)
-      : "NOTHING HERE YET!"
-    }
+        ? projects.forEach(project => <ProjectThumb project={project}/>)
+        : "NOTHING HERE YET!"
+      }
     </div>
   )
 }
